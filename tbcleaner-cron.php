17e8580a4c1d8688b73101591d7ca816d69fef0d
Yabbertb <144372521+Yabbertb@users.noreply.github.com>
2025-03-11 11:11:01 +0100

    Table cleanup and CRON cleaner.

---
+++ tbcleaner-cron.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 2007-2016 PrestaShop
+ *
+ * Thirty Bees is an extension to the PrestaShop e-commerce software developed by PrestaShop SA
+ * Copyright (C) 2017-2024 thirty bees
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@thirtybees.com so we can send you a copy immediately.
+ *
+ * @author    Thirty Bees <modules@thirtybees.com>
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2017-2024 thirty bees
+ * @copyright 2007-2016 PrestaShop SA
+ * @license   https://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
+ */
+
+/** @noinspection PhpUnhandledExceptionInspection */
+
+include __DIR__.'/../../config/config.inc.php';
+include __DIR__.'/../../init.php';
+/* Check to security tocken */
+if (substr(Tools::encrypt('tbcleaner/cron'), 0, 10) != Tools::getValue('token') || !Module::isInstalled('tbcleaner')) {
+    die('Bad token');
+}
+/** @var TbCleaner $tbcleaner */
+$tbcleaner = Module::getInstanceByName('tbcleaner');
+/* Check if the module is enabled */
+if ($tbcleaner->active) {
+    $tbcleaner->cleanAndOptimize();
+}
